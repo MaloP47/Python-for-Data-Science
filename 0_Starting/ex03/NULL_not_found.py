f4e771406b3d2ebdbbb578845ea44b4d4d8123fc
MaloP47 <mpeulet@gmail.com>
2024-07-26 15:59:36 +0200

    ex03 complete

--- 0_Starting/ex03/NULL_not_found.py
+++ 0_Starting/ex03/NULL_not_found.py
@@ -1,2 +1,20 @@
+import math
+
 def NULL_not_found(object: any) -> int:
-	
+	if object is None:
+		print(f'Nothing: None {type(object)}')
+	elif isinstance(object, float) and math.isnan(object):
+		print(f'Cheese: {object} {type(object)}')
+	elif isinstance(object, int) and object is 0:
+		print(f'Zero: {object} {type(object)}')
+	elif isinstance(object, str) and not len(object):
+		print(f'Empty: {object} {type(object)}')
+	elif isinstance(object, bool) and object is False:
+		print(f'Fake: {object} {type(object)}')
+	# else:
+	# 	print(f'Anything else: {object} {type(object)}')
+	# 	return 1
+	else:
+		print('Type not found')
+		return 1
+	return 0
--- 0_Starting/ex03/tester.py
+++ 0_Starting/ex03/tester.py
@@ -1,11 +1,21 @@
 from NULL_not_found import NULL_not_found
+
 Nothing = None
 Garlic = float("NaN")
+bread = float(3.14)
 Zero = 0
-Empty = ’’
+Empty = ''
 Fake = False
+
+print('########################')
+print(Garlic)
+print(type(Garlic))
+print(Garlic == Garlic)
+print('########################')
+
 NULL_not_found(Nothing)
 NULL_not_found(Garlic)
+print(NULL_not_found(bread))
 NULL_not_found(Zero)
 NULL_not_found(Empty)
 NULL_not_found(Fake)
