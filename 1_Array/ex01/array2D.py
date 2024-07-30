32c1f02f7d064aaf260f2d4f0c28ad4daaab06e4
MaloP47 <mpeulet@gmail.com>
2024-07-30 20:51:44 +0200

    1_Array ex01 complete

---
+++ 1_Array/ex01/array2D.py
@@ -0,0 +1,23 @@
+"""1_Array ex01"""
+
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """Prints shape & returns sliced version"""
+
+    if not isinstance(family, list):
+        raise AssertionError("wrong type of container")
+
+    if not isinstance(start, int) or not isinstance(end, int):
+        raise AssertionError("wrong type of arguments")
+
+    if any(not isinstance(item, list) or len(item) != len(family[0])
+            for item in family):
+        raise AssertionError("must be a 2D array")
+
+    Shape = np.array(family)
+    print(f'My shape is : {Shape.shape}')
+    newShape = Shape[start:end]
+    print(f'My new shape is : {newShape.shape}')
+    return newShape.tolist()
