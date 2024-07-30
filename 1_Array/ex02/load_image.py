6296afdbb4126e625a97c1d01f1a35588fde49a6
MaloP47 <mpeulet@gmail.com>
2024-07-30 23:14:28 +0200

    Prepared 1_Array ex02, adjusted all raise to generic Exceptions instead of Assertion

---
+++ 1_Array/ex02/load_image.py
@@ -0,0 +1,10 @@
+"""1_Array ex02"""
+
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str) -> list:
+    """Load a .JPEG or .JPG, prints format and pixels in RGB"""
+
+    return []
