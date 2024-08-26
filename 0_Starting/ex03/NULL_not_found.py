e2ff89699209d442a03cc3ecd2c6095783e153d3
Mallory PEULET <mpeulet@gmail.com>
2024-08-26 11:39:57 +0200

    Used comparative class type not avoid using function

--- 0_Starting/ex03/NULL_not_found.py
+++ 0_Starting/ex03/NULL_not_found.py
@@ -1,20 +1,41 @@
 import math
 
 
+# def NULL_not_found(object: any) -> int:
+#     if object is None:
+#         print(f'Nothing: None {type(object)}')
+#     elif isinstance(object, float) and math.isnan(object):
+#         print(f'Cheese: {object} {type(object)}')
+#     elif isinstance(object, int) and object == 0:
+#         print(f'Zero: {object} {type(object)}')
+#     elif isinstance(object, str) and not len(object):
+#         print(f'Empty: {object} {type(object)}')
+#     elif isinstance(object, bool) and object is False:
+#         print(f'Fake: {object} {type(object)}')
+#     # else:
+#     # 	print(f'Anything else: {object} {type(object)}')
+#     # 	return 1
+#     else:
+#         print('Type not found')
+#         return 1
+#     return 0
+
+import math
+
 def NULL_not_found(object: any) -> int:
     if object is None:
         print(f'Nothing: None {type(object)}')
-    elif isinstance(object, float) and math.isnan(object):
+    elif type(object) == float and math.isnan(object):
         print(f'Cheese: {object} {type(object)}')
-    elif isinstance(object, int) and object == 0:
+    elif type(object) == int and object == 0:
         print(f'Zero: {object} {type(object)}')
-    elif isinstance(object, str) and not len(object):
+    elif type(object) == str and not len(object):
         print(f'Empty: {object} {type(object)}')
-    elif isinstance(object, bool) and object is False:
+    elif type(object) == bool and object is False:
         print(f'Fake: {object} {type(object)}')
-    # else:
-    # 	print(f'Anything else: {object} {type(object)}')
-    # 	return 1
+#     # else:
+#     # 	print(f'Anything else: {object} {type(object)}')
+#     # 	return 1
     else:
         print('Type not found')
         return 1
